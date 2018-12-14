beabf3954e6ab3dc0eecf4e5dcf5f6e53cc93d57
平石匡 <masahiraishi22@gmail.com>
2018-12-14 11:11:20 +0900

    commit

--- resources/views/bbc/index.blade.php
+++ resources/views/bbc/index.blade.php
@@ -8,12 +8,11 @@
             <h2>タイトル:{{$post->title}}
                 <small>投稿日:{{date("Y年 m月 d日",strtotime($post->created_at))}}</small>
             </h2>
-{{--            <p>カテゴリー:{{$post->category->name}}</p>--}}
             <p>カテゴリー:<a href="{{url('bbc/category',$post->category->id)}}">{{$post->category->name}}</a></p>
             <p>{{$post->content}}</p>
             <p><a href="{{url('bbc',$post->id)}}" class="btn btn-primary">続きを読む</a></p>
             <p>コメント数:{{$post->comment_count}}</p>
-
         @endforeach
+{{--        {{$posts->links()}}--}}
     </div>
 @endsection
